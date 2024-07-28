9c8ce66001fdd321bff3595a708b133ea2050510
Woo-Sun Yang <wyang@lbl.gov>
2024-07-28 09:42:40 -0700

    Update scripts for using Nsight Compute

--- testcases/conus_12-km/wrapper-ncu.sh
+++ testcases/conus_12-km/wrapper-ncu.sh
@@ -2,9 +2,7 @@
  output=report.%q{SLURM_PROCID}.%q{SLURM_JOBID}
 
  if [[ ${SLURM_PROCID} == "0" ]] ; then
-   dcgmi profile --pause
    ncu --target-processes all --kernel-id :::1 -o ${output} "$@"
-   dcgmi profile --resume
  else
    "$@"
  fi
